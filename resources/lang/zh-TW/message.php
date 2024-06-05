6a8d3c953b7de34f621ac0d0fa926f027efc68f2
JACK LIN <cvse00566@gmail.com>
2024-06-05 22:47:51 +0800

    登入頁中文化

---
+++ resources/lang/zh-TW/message.php
@@ -0,0 +1,10 @@
+<!-- //zh-TW/message.php -->
+<?php
+
+return [
+    'Email' => '電子信箱',
+    'Password' => '密碼',
+    'Remember me' => '記住我',
+    'Forgot your password' => '忘記密碼',
+    'Log in' => '登入',
+];
